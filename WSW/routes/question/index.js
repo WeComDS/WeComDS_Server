fbfb705eac9203f8e49361e866a9964c74f85c10
Tae Young <k55416325@gamil.com>
2021-08-18 01:03:11 +0900

    각 게시판 별 POST, GET, DELTE까지 구현

--- WSW/routes/question/index.js
+++ WSW/routes/question/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const controller = require('./question.controller')
 
+//질문게시판 목록
+router.get('/', controller.question);
+//질문게시판 글 쓰기
+router.post('/write', controller.write);
+//질문게시판 글 삭제
+router.delete('/:id', controller.delete);
 
 
 module.exports = router;
--- WSW/routes/secret/secret.controller.js
+++ WSW/routes/secret/secret.controller.js
@@ -27,16 +27,64 @@ exports.getAll = (req, res) => {
 
 }
 /* 글 조회 */
-exports.get = (req, res) => {
-
+exports.secret = (req, res) => {
+  console.log('secret_list');
+    
+    let sql = 'SELECT * FROM secret;'
+    connection.query(sql,
+      (err,rows,fiedls)=>{
+        res.send(rows)
+      }
+    )
 }
 
 /* 등록하기 */
-exports.post = (req, res) => {
+exports.write = (req,res) => {
+  console.log('secret_write');
 
+  const user_id = req.body.user_id;
+  const sec_content = req.body.sec_content;
+  const sec_date = req.body.sec_date;
+  const sec_like = req.body.sec_like;
+  const sec_CMcount = req.body.sec_CMcount;
+
+  let params = [user_id, sec_content, sec_date, sec_like, sec_CMcount]
+  let sql = 'INSERT INTO secret(user_id, sec_content, sec_date, sec_like, sec_CMcount) VALUES(?,?,?,?,?)'
+
+  connection.query(sql, params,
+    (err, rows, fiedls) => {
+      console.log(err);
+      if(err){
+        console.log('secret write err')
+        res.send(rows)
+      }
+      else{
+        console.log('secret write success')
+        res.send(rows)
+      }
+    }  
+  )
 }
+
 
 /* 삭제하기 */
 exports.delete = (req, res) => {
+  console.log('secret_delete');
+  
+  const sec_id = req.query.sec_id;
 
+  let sql = 'delete from secret where sec_id=?'
+
+  connection.query(sql, sec_id,
+    (err, rows, fiedls) => {
+      console.log(err);
+      if(err){
+        console.log('secret delete err')
+        res.send(rows)
+      }else{
+        console.log('secret delete success')
+        res.send(rows)
+      }
+    }  
+  )
 }
